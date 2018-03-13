cc09e221acfa7fc9085d344f13496a126e599acb
Martin Jaros <jaros@arion.cz>
2018-03-13 20:34:50 +0100

    Recompile for KSP 1.4.1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # KSP-WhoAmI
 
 Identify your kerbals in IVA
+
+# Changelog
+## Version 1.0.5
+- Recompile for KSP 1.4.1
--- WhoAmI/WhoAmI.cs
+++ WhoAmI/WhoAmI.cs
@@ -3,59 +3,59 @@ using UnityEngine;
 
 namespace WhoAmI
 {
-	[KSPAddon (KSPAddon.Startup.Flight, false)]
-	public class WhoAmI: MonoBehaviour
-	{
-		void Awake ()
-		{
-			GameEvents.OnIVACameraKerbalChange.Add (OnIVACameraKerbalChange);
-			GameEvents.OnCameraChange.Add (OnCameraChange);
-		}
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class WhoAmI : MonoBehaviour
+    {
+        void Awake()
+        {
+            GameEvents.OnIVACameraKerbalChange.Add(OnIVACameraKerbalChange);
+            GameEvents.OnCameraChange.Add(OnCameraChange);
+        }
 
-		void OnDestroy ()
-		{
-			GameEvents.OnIVACameraKerbalChange.Remove (OnIVACameraKerbalChange);
-			GameEvents.OnCameraChange.Remove (OnCameraChange);
-		}
+        void OnDestroy()
+        {
+            GameEvents.OnIVACameraKerbalChange.Remove(OnIVACameraKerbalChange);
+            GameEvents.OnCameraChange.Remove(OnCameraChange);
+        }
 
-		public void Update ()
-		{
-			if (Input.GetKeyDown (KeyCode.B) &&
-				GameSettings.MODIFIER_KEY.GetKey() &&
-			    CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.IVA &&
-				CrewHatchController.fetch.CrewDialog == null)
-			{
-				CrewHatchController.fetch.SpawnCrewDialog (
-					CameraManager.Instance.IVACameraActiveKerbal.InPart,
-					false,
-					true
-				);
-			}
-		}
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.B) &&
+                GameSettings.MODIFIER_KEY.GetKey() &&
+                CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.IVA &&
+                CrewHatchController.fetch.CrewDialog == null)
+            {
+                CrewHatchController.fetch.SpawnCrewDialog(
+                    CameraManager.Instance.IVACameraActiveKerbal.InPart,
+                    false,
+                    true
+                );
+            }
+        }
 
-		private void OnCameraChange (CameraManager.CameraMode cameraMode)
-		{
-			if (cameraMode == CameraManager.CameraMode.IVA)
-				ShowInfo ();
-		}
+        private void OnCameraChange(CameraManager.CameraMode cameraMode)
+        {
+            if (cameraMode == CameraManager.CameraMode.IVA)
+                ShowInfo();
+        }
 
-		private void OnIVACameraKerbalChange (Kerbal kerbal)
-		{
-			ShowInfo ();
-		}
+        private void OnIVACameraKerbalChange(Kerbal kerbal)
+        {
+            ShowInfo();
+        }
 
-		private void ShowInfo ()
-		{
-			Kerbal kerbinaut = CameraManager.Instance.IVACameraActiveKerbal;
-			ScreenMessages.PostScreenMessage (
-				String.Format (
-					"{0} - {1} Level {2}\n({3})",
-					kerbinaut.crewMemberName,
-					kerbinaut.protoCrewMember.experienceTrait.TypeName,
-					kerbinaut.protoCrewMember.experienceLevel,
-					kerbinaut.InPart.partInfo.title
-				)
-			);
-		}
-	}
+        private void ShowInfo()
+        {
+            Kerbal kerbinaut = CameraManager.Instance.IVACameraActiveKerbal;
+            ScreenMessages.PostScreenMessage(
+                String.Format(
+                    "{0} - {1} Level {2}\n({3})",
+                    kerbinaut.crewMemberName,
+                    kerbinaut.protoCrewMember.experienceTrait.TypeName,
+                    kerbinaut.protoCrewMember.experienceLevel,
+                    kerbinaut.InPart.partInfo.title
+                )
+            );
+        }
+    }
 }
