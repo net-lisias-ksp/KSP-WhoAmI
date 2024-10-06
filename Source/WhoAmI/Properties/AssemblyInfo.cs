b80d9c59cec919e67ef7a214c06a3099d6fa136c
Lisias T <me@lisias.net>
2024-10-06 04:50:24 -0300

    MOAR Legal Mambo Jambo

--- Source/WhoAmI/Properties/AssemblyInfo.cs
+++ Source/WhoAmI/Properties/AssemblyInfo.cs
@@ -4,19 +4,21 @@
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle ("WhoAmI")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("Identify your Kerbals in IVA.")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("realgecko")]
-[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCompany (WhoAmI.LegalMamboJambo.Company)]
+[assembly: AssemblyProduct (WhoAmI.LegalMamboJambo.Product)]
+[assembly: AssemblyCopyright (WhoAmI.LegalMamboJambo.Copyright)]
+[assembly: AssemblyTrademark (WhoAmI.LegalMamboJambo.Trademark)]
 [assembly: AssemblyCulture ("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.4.0.0")]
+[assembly: AssemblyVersion(WhoAmI.Version.Number)]
+[assembly: AssemblyFileVersion(WhoAmI.Version.Number)]
+[assembly: KSPAssembly("WhoAmI", WhoAmI.Version.major, WhoAmI.Version.minor)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
---
+++ Source/WhoAmI/Startup.cs
@@ -0,0 +1,29 @@
+﻿/*
+	This file is part of Who Am I? /L
+		© 2024 LisiasT : http://lisias.net <support@lisias.net>
+
+	Who Am I? /L is licensed as follows:
+		* GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
+
+	Who Am I? /L is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the GNU General Public License 3.0
+	along with Who Am I? /L. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System;
+using UnityEngine;
+
+namespace WhoAmI
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+	internal class Startup:MonoBehaviour
+	{
+		private void Start()
+		{
+			Log.force("Version {0}", Version.Text);
+		}
+	}
+}
